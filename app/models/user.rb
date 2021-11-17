427062085f8429259e34136835b9a12abedbd59e
nori35 <spzx34e@yahoo.co.jp>
2021-11-17 21:23:38 +0900

    コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,7 @@ class User < ApplicationRecord
   validates :nickname, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
 
   with_options presence: true do
-    # ひらがな、カタカナ、漢字のみ許可する
     validates :name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    # カタカナのみ許可する
     validates :name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
 end
