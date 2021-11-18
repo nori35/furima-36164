b29c0f9d88cac2a48e9dc3235fb9f7646b32668d
nori35 <spzx34e@yahoo.co.jp>
2021-11-18 09:19:44 +0900

    誕生日、バリデーション指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :email, presence: true
-
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
@@ -15,4 +13,6 @@ class User < ApplicationRecord
     validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
+
+  validates :birthday, presence: true
 end
