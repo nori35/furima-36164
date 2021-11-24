814146720edddd58fcaab0fc62abf66673cbbff6
nori35 <spzx34e@yahoo.co.jp>
2021-11-24 19:34:52 +0900

    bot設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    title {"テスト"}
+    explanation {"問題なし"}
+    category_id {Category.all.sample}
+    product_condition_id {ProductCondition.all.sample}
+    shipping_charges_id {ShippingCharges.all.sample}
+    shipping_area_id {ShippingArea.all.sample}
+    days_to_ship_id {DaysToShip.all.sample}
+    price {"10000"}
   end
 end
