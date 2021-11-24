38776b44144361b0562b217ea5c36636a46e6521
nori35 <spzx34e@yahoo.co.jp>
2021-11-24 11:12:08 +0900

    パラムスの指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,22 @@ class ItemsController < ApplicationController
     @items = Article.order("created_at DESC")
   end
 
+  def new
+    @item = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def items_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:content, :image, :category_id, :product_condition_id, :shipping_charges_id, :shipping_area_id, :bays_to_ship_id).merge(user_id: current_user.id)
   end
 end
