b979eb4cff6963f2c22663ac21c907d06f0b639f
nori35 <spzx34e@yahoo.co.jp>
2021-11-30 09:47:08 +0900

    ぼっと作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     title {"テスト"}
     explanation {"問題なし"}
-    category_id {Category.all.sample}
-    product_condition_id {ProductCondition.all.sample}
-    shipping_charges_id {ShippingCharges.all.sample}
-    shipping_area_id {ShippingArea.all.sample}
-    days_to_ship_id {DaysToShip.all.sample}
+    category_id {2}
+    product_condition_id {2}
+    shipping_charges_id {2}
+    shipping_area_id {2}
+    days_to_ship_id {2}
     price {"10000"}
   end
 end
