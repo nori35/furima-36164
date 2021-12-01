945f7e5f5697af95005e5d285d58c945df5eea63
nori35 <spzx34e@yahoo.co.jp>
2021-12-01 17:38:12 +0900

    不要

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,7 @@
 window.addEventListener('load', () => {
-  console.log("OK");
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
     const addPofitDom = document.getElementById("profit");
