8047d9c2170052288b31a31f247d42a313769efa
nori35 <spzx34e@yahoo.co.jp>
2021-12-01 17:45:28 +0900

    価格上限テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -71,6 +71,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
+      it "価格は¥300~¥9,999,999の間でないとないと保存できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
     end
   end
 end
