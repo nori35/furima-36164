93aeb6bd9955e69a917039d8cdbb9af546e45606
nori35 <spzx34e@yahoo.co.jp>
2021-12-01 16:04:09 +0900

    インスタンス変数の定義修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -10,7 +10,3 @@ window.addEventListener('load', () => {
     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
   })
 });
-// 料金の表示
-// 値段を入力すると、その値段をJSで取得する
-// その取得した値段を元に計算を行う
-// その計算した手数料などをJSを使って表示
