7f9db94c3c0f4feac8ad574cbb66b1610f8e4ccb
nori35 <spzx34e@yahoo.co.jp>
2021-12-01 11:31:52 +0900

    priceテストコード成功

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,13 +10,16 @@ RSpec.describe Item, type: :model do
       it "image、title、explanation、category_id、product_condition_id、shipping_charges_id、shipping_area_id、days_to_ship_id、priceが存在すれば登録できる" do
         expect(@item).to be_valid
       end
+      it "priceが半角数字の設定であれば登録できる" do
+        @item.price = "10000"
+        expect(@user).to be_valid
+      end
     end
     context "出品登録できないとき" do
       it "imageが空だと登録できない" do
-        @item.image = ""
+        @item.image = nil
         @item.valid?
-        binding.pry
-        expect(@item.errors.full_messages).to include("Content can't be blank")
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it "titleが空だと登録できない" do
         @item.title = ""
@@ -56,6 +59,12 @@ RSpec.describe Item, type: :model do
       it "priceが空だと登録できない" do
         @item.price = ""
         @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "価格は半角数値ののみでないとないと保存できない" do
+        @item.price = "１２３４５６"
+        @item.valid?
+        binding.pry
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
     end
