34afb4c97b7bcb13997f9ec7e0ab4a2ff278dcb6
nori35 <spzx34e@yahoo.co.jp>
2021-12-01 15:50:38 +0900

    手数料計算

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,16 @@
 window.addEventListener('load', () => {
   console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+    const addPofitDom = document.getElementById("profit");
+    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+  })
 });
+// 料金の表示
+// 値段を入力すると、その値段をJSで取得する
+// その取得した値段を元に計算を行う
+// その計算した手数料などをJSを使って表示
