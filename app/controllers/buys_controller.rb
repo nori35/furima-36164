c1005ab3c5339fc2ee8b6b5718b02d836c7342a6
nori35 <spzx34e@yahoo.co.jp>
2021-12-11 21:19:29 +0900

    ストロングスパラメーター

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -5,4 +5,18 @@ class BuysController < ApplicationController
 
   def create
   end
+  def create
+    @buy_shipping_addresse = BuyShippingAddress.new(buy_params)
+    if @buy_address.valid?
+       @buy_address.save
+       redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def buy_params
+    params.permit(:postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :item_id)
+  end
 end
