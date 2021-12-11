c08042fee2cf1976c29d448c2d94d3ed238b2e83
nori35 <spzx34e@yahoo.co.jp>
2021-12-11 21:50:25 +0900

    電話番号バリデーション修正

--- app/models/buy_shipping_addresse.rb
+++ app/models/buy_shipping_addresse.rb
@@ -7,7 +7,7 @@ class BuyShippingAddresse
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :address
-    validates :telephone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :telephone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
     validates :number
     validates :buy
   end
