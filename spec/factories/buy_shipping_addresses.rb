ceb6d0b583812c263ab5bd146f1f72b6be918cbd
nori35 <spzx34e@yahoo.co.jp>
2021-12-17 16:58:59 +0900

    テストコード

---
+++ spec/factories/buy_shipping_addresses.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :buy_shipping_addresse do
+    postal_code {"123-4567"}
+    shipping_area_id {"2"}
+    city {"札幌市"}
+    address {"西1-1"}
+    telephone_number {"09012345678"}
+  end
+end
