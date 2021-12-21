979d66b302abb955209ad95a509d028e4715e5b5
nori35 <spzx34e@yahoo.co.jp>
2021-12-21 11:45:26 +0900

    カード情報

---
+++ app/javascript/card.js
@@ -0,0 +1,28 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_2c832e9d0832b7947e2283e4");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+    console.log(card);
+    debugger;
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
