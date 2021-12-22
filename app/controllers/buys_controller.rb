270f893a542a539e9e499da1e49eda6246c67438
nori35 <spzx34e@yahoo.co.jp>
2021-12-22 22:07:25 +0900

    詳細完売表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -9,7 +9,11 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_shipping_addresses = BuyShippingAddresse.new(buy_params)
     if @buy_shipping_addresses.valid?
-      pay_item
+      Payjp.api_key = "sk_test_050f9d142d7918a0d599157a"
+      Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_params[:token],
+      currency: 'jpy')
       @buy_shipping_addresses.save
       redirect_to root_path
     else
@@ -20,16 +24,16 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.require(:buy_shipping_addresse).permit(:postal_code, :shipping_area_id, :city, :address, :building_name, :telephone_number, :item_id).merge(token: params[:token], user_id: current_user.id)
+    params.require(:buy_shipping_addresse).permit(:postal_code, :shipping_area_id, :city, :address, :building_name, :telephone_number).merge(item_id: @item.id, token: params[:token], user_id: current_user.id)
   end
 
-  def pay_item
-    Payjp.api_key = "sk_test_050f9d142d7918a0d599157a"
-    Payjp::Charge.create(
-      amount: @item.price,
-      card: buy_params[:token],
-      currency: 'jpy'
-    )
-  end
+  # def pay_item
+  #   Payjp.api_key = "sk_test_050f9d142d7918a0d599157a"
+  #   Payjp::Charge.create(
+  #     amount: @item.price,
+  #     card: buy_params[:token],
+  #     currency: 'jpy'
+  #   )
+  # end
 
 end
