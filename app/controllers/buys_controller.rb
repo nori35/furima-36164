17898bd735c36ee3929180603818f99ab45af7ac
nori35 <spzx34e@yahoo.co.jp>
2021-12-24 22:27:02 +0900

    商品投稿者、別ユーザー購入画面飛ばない

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,14 +1,15 @@
 class BuysController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :prevent_url, only: [:index]
+  before_action :contributor_confirmation, only: [:index]
 
-  
   def index
     @buy = Buy.all.order(created_at: :desc)
     @buy_shipping_addresses = BuyShippingAddresse.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buy_shipping_addresses = BuyShippingAddresse.new(buy_params)
     if @buy_shipping_addresses.valid?
       Payjp.api_key = "sk_test_050f9d142d7918a0d599157a"
@@ -29,13 +30,20 @@ class BuysController < ApplicationController
     params.require(:buy_shipping_addresse).permit(:postal_code, :shipping_area_id, :city, :address, :building_name, :telephone_number).merge(item_id: @item.id, token: params[:token], user_id: current_user.id)
   end
 
-  # def pay_item
-  #   Payjp.api_key = "sk_test_050f9d142d7918a0d599157a"
-  #   Payjp::Charge.create(
-  #     amount: @item.price,
-  #     card: buy_params[:token],
-  #     currency: 'jpy'
-  #   )
-  # end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def prevent_url
+    if @item.user_id != current_user.id
+    redirect_to root_path
+    end
+  end
+
+  def contributor_confirmation
+    if current_user == @item.user
+    redirect_to root_path 
+    end
+  end
 
 end
